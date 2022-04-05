340504856d11ffaba54bc2196bc9ca654fdb6069
Big Large <maxwell.lee.372007@gmail.com>
2022-04-04 21:04:21 -0400

    PROG-262 base ball jam code

--- src/main/java/com/lightningrobotics/voidrobot/commands/indexer/RunIndexer.java
+++ src/main/java/com/lightningrobotics/voidrobot/commands/indexer/RunIndexer.java
@@ -2,7 +2,9 @@ package com.lightningrobotics.voidrobot.commands.indexer;
 
 import java.util.function.DoubleSupplier;
 
+import com.lightningrobotics.voidrobot.subsystems.HubTargeting;
 import com.lightningrobotics.voidrobot.subsystems.Indexer;
+import com.lightningrobotics.voidrobot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -10,13 +12,16 @@ public class RunIndexer extends CommandBase {
 
     // Creates the indexer subsystem
     private final Indexer indexer;
-
+    private final HubTargeting targeting;
     // The power we want to supply to the indexer
     private DoubleSupplier power;
 
-    public RunIndexer(Indexer indexer, DoubleSupplier power) {
+    
+
+    public RunIndexer(Indexer indexer, HubTargeting targeting, DoubleSupplier power) {
         this.indexer = indexer;
         this.power = power; // The power that is comming from the controller
+        this.targeting = targeting;
 
         addRequirements(indexer);
     }
@@ -26,7 +31,12 @@ public class RunIndexer extends CommandBase {
 
     @Override
     public void execute() {
-        indexer.setPower(power.getAsDouble()); // Gets the supplied power, and sets it to the indexer motor
+        if (indexer.getExitStatus() && targeting.onTarget(2000, 0, 0) && power.getAsDouble() > 0) {
+            indexer.setPower(0);
+        }
+        else {
+            indexer.setPower(power.getAsDouble()); // Gets the supplied power, and sets it to the indexer motor
+        }
     }
 
     @Override
